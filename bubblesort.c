94c35d2d5dd024794c89d8c6e4f3059678b9053e
samjosaji <samjosaji@gmail.com>
2024-08-28 13:43:59 +0530

    Update bubblesort.c

--- bubblesort.c
+++ bubblesort.c
@@ -1,41 +1,45 @@
-#include <stdio.h>
-  void main()
-{
-  int a[100], n, i, j,temp,c=0 ;
-  c++;
-  printf("Enter number of elements:\n");
-  c++;
-  scanf("%d", &n);
-  c++;
-  printf("Enter the elements:\n");
-  c+2;
-  for (i= 0;i< n;i++){
-    scanf("%d", &a[i]);
-    c++;
-    c++;
-   }
-   c++;
-  for (i=0;i< n - 1;i++)
-  {
-      c+2;
-    for (j= 0 ;j< n-i-1;j++)
-    {
-        c++;
-      if (a[j]> a[j+1])
-      {
-         temp= a[j];c++;
-        a[j]= a[j+1];c++;
-        a[j+1] =temp;c++;
-      }}}
-  printf("Sorted is:\t");
-  c+2;
-  for (i = 0;i< n;i++){
-        c++;
-  printf("%d\t",a[i]);
-  c++;
-  }
-  c++;
-  printf("\nthe space complexity is %d\n",25+(n*4));
-  c++;
-  printf("the time complexity is %d\n",c);
-}
+#include <stdio.h> 
+void main() 
+{ 
+  int a[100], n, i, j,temp,t=0 ; 
+  t++; 
+  printf("Enter number of elements:\n"); 
+  t++; 
+  scanf("%d", &n); 
+  t++; 
+  printf("Enter the elements:\n"); 
+  t+2; 
+  for (i= 0;i< n;i++) 
+  { 
+    scanf("%d", &a[i]); 
+    t++; 
+    t++; 
+  } 
+  t++; 
+  for (i=0;i< n - 1;i++) 
+  { 
+    t+2; 
+    for (j= 0 ;j< n-i-1;j++) 
+    { 
+      t++; 
+      if (a[j]> a[j+1]) 
+      { 
+        temp= a[j];t++; 
+        a[j]= a[j+1];t++;
+        a[j+1] =temp;t++; 
+      } 
+    } 
+  } 
+  printf("Sorted is:\t"); 
+  t+2; 
+  for (i = 0;i< n;i++) 
+  { 
+    t++; 
+    printf("%d\t",a[i]); 
+    t++; 
+  } 
+  t++; 
+  printf("\nthe space complexity is %d\n",25+(n*4)); 
+  t++; 
+  printf("the time complexity is %d\n",t); 
+} 
